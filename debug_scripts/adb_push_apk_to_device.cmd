40186f9a76d16670efc287d3029ce13d477d52b8
Catfriend1 <16361913+Catfriend1@users.noreply.github.com>
2020-01-11 14:50:31 +0100

    Create adb_push_apk_to_device.cmd

---
+++ debug_scripts/adb_push_apk_to_device.cmd
@@ -0,0 +1,23 @@
+@echo off
+cls
+cd /d "%~dps0"
+REM 
+REM Purpose
+REM 	Installs the current debug APK built by Android studio or gradlew to a physical device via ADB.
+REM 
+SET APK_FULLFN="..\app\build\outputs\apk\debug\app-debug.apk"
+SET APK_PACKAGE_NAME=com.github.catfriend1.syncthingandroid.debug
+SET APK_USER_INSTALL=
+REM SET APK_USER_INSTALL=--user 0
+REM 
+REM Check prerequisites
+IF NOT EXIST "%APK_FULLFN%" echo [ERROR] APK_FULLFN=[%APK_FULLFN%] does not exist. Stop. & pause & goto :eof
+REM 
+echo [INFO] Installing APK to attached USB device ...
+adb install -r %APK_USER_INSTALL% "%APK_FULLFN%"
+REM 
+echo [INFO] Starting app ...
+adb shell monkey -p "%APK_PACKAGE_NAME%" 1
+REM 
+timeout 2
+goto :eof
