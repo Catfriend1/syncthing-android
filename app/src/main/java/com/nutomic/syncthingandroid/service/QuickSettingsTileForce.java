07d91f12e4a13a88d8cbf029738c7ee919c81675
Helium314 <43007630+Helium314@users.noreply.github.com>
2021-04-04 00:48:42 +0200

    Add Quick Setting Tiles (fixes #444) (#768)
    * add QS tiles in manifest
    
    icons and proper label strings missing
    long click on tiles will start MainActivity
    
    * add force-start QS tile
    
    * add QS tile for start/stop of scheduled runs
    
    * add proper qs tile cons and labels
    
    * add qs tile icons
    
    * update QS schedule tile
    
    * add strings for QS tiles
    
    * update QS force tile
    
    * update qs tile icon
    
    * set tile unvavailable if force run/stop is active
    
    * disable qs tile if app not running
    
    * disable qs tiles if app not running
    
    * update force buttons when changed using qs tile
    
    * don't search through all activities if syncthing was found
    
    * don't search through all activities if syncthing was found
    
    * fix code formatting
    
    * update strings
    
    * add qs tile icons for stopped/running state
    
    * set force-tile icons according to run state
    
    * remove outdated comment (already fixed)
    
    * add comments to force tile
    
    * add comments to schedule-tile
    
    Co-authored-by: Catfriend1 <Catfriend1@users.noreply.github.com>

---
+++ app/src/main/java/com/nutomic/syncthingandroid/service/QuickSettingsTileForce.java
@@ -0,0 +1,111 @@
+package com.nutomic.syncthingandroid.service;
+
+import android.app.ActivityManager;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.res.Resources;
+import android.graphics.drawable.Icon;
+import android.os.Build;
+import android.service.quicksettings.Tile;
+import android.service.quicksettings.TileService;
+
+import androidx.annotation.RequiresApi;
+import androidx.localbroadcastmanager.content.LocalBroadcastManager;
+import androidx.preference.PreferenceManager;
+
+import com.nutomic.syncthingandroid.R;
+
+import javax.inject.Inject;
+
+import static com.nutomic.syncthingandroid.service.RunConditionMonitor.ACTION_UPDATE_SHOULDRUN_DECISION;
+
+@RequiresApi(api = Build.VERSION_CODES.N)
+public class QuickSettingsTileForce extends TileService {
+    public QuickSettingsTileForce() {
+
+    }
+    private Context mContext;
+    @Inject
+    SharedPreferences mPreferences;
+    private Resources res;
+
+    @Override
+    public void onStartListening() {
+        Tile tile = getQsTile();
+        if (tile != null) {
+            mContext = getApplication().getApplicationContext();
+            res = mContext.getResources();
+            mPreferences = PreferenceManager.getDefaultSharedPreferences(mContext);
+
+            // search through running services to see whether the app is currently running
+            ActivityManager am = (ActivityManager) getSystemService(Context.ACTIVITY_SERVICE);
+            boolean syncthingRunning = false;
+            for (ActivityManager.RunningServiceInfo service : am.getRunningServices(Integer.MAX_VALUE)) {
+                if (SyncthingService.class.getName().equals(service.service.getClassName())) {
+                    syncthingRunning = true;
+                    break;
+                }
+            }
+            // disable tile if app is not running
+            if (!syncthingRunning) {
+                tile.setState(Tile.STATE_UNAVAILABLE);
+                tile.updateTile();
+                return;
+            }
+
+            // update tile to reflect forced-state
+            updateTileState(tile, mPreferences.getInt(Constants.PREF_BTNSTATE_FORCE_START_STOP, Constants.BTNSTATE_NO_FORCE_START_STOP));
+        }
+        super.onStartListening();
+    }
+
+    @Override
+    public void onClick() {
+        Tile tile = getQsTile();
+        int newState;
+        switch (mPreferences.getInt(Constants.PREF_BTNSTATE_FORCE_START_STOP, Constants.BTNSTATE_NO_FORCE_START_STOP)) {
+            case Constants.BTNSTATE_FORCE_START:
+                newState = Constants.BTNSTATE_FORCE_STOP;
+                break;
+            case Constants.BTNSTATE_NO_FORCE_START_STOP:
+                newState = Constants.BTNSTATE_FORCE_START;
+                break;
+            case Constants.BTNSTATE_FORCE_STOP:
+            default:
+                newState = Constants.BTNSTATE_NO_FORCE_START_STOP;
+        }
+        SharedPreferences.Editor editor = mPreferences.edit();
+        editor.putInt(Constants.PREF_BTNSTATE_FORCE_START_STOP, newState);
+        editor.apply();
+
+        LocalBroadcastManager localBroadcastManager = LocalBroadcastManager.getInstance(mContext);
+        Intent intent = new Intent(ACTION_UPDATE_SHOULDRUN_DECISION);
+        localBroadcastManager.sendBroadcast(intent);
+
+        updateTileState(tile, newState);
+        tile.updateTile();
+    }
+
+    private void updateTileState(Tile tile, int force) {
+        switch (force) {
+            case Constants.BTNSTATE_FORCE_START:
+                tile.setLabel(res.getString(R.string.qs_forced_to_run));
+                tile.setState(Tile.STATE_ACTIVE);
+                tile.setIcon(Icon.createWithResource(mContext,R.drawable.ic_qs_forced_to_run));
+                break;
+            case Constants.BTNSTATE_FORCE_STOP:
+                tile.setLabel(res.getString(R.string.qs_forced_to_stop));
+                tile.setState(Tile.STATE_ACTIVE);
+                tile.setIcon(Icon.createWithResource(mContext,R.drawable.ic_qs_forced_to_stop));
+                break;
+            case Constants.BTNSTATE_NO_FORCE_START_STOP:
+            default:
+                tile.setLabel(res.getString(R.string.qs_following_run_conditions));
+                tile.setState(Tile.STATE_INACTIVE);
+                tile.setIcon(Icon.createWithResource(mContext,R.drawable.ic_qs_force));
+        }
+        tile.updateTile();
+    }
+
+}
---
+++ app/src/main/java/com/nutomic/syncthingandroid/service/QuickSettingsTileSchedule.java
@@ -0,0 +1,74 @@
+package com.nutomic.syncthingandroid.service;
+
+import android.app.ActivityManager;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.res.Resources;
+import android.os.Build;
+import android.service.quicksettings.Tile;
+import android.service.quicksettings.TileService;
+
+import androidx.annotation.RequiresApi;
+import androidx.localbroadcastmanager.content.LocalBroadcastManager;
+import androidx.preference.PreferenceManager;
+
+import com.nutomic.syncthingandroid.R;
+
+import javax.inject.Inject;
+
+import static com.nutomic.syncthingandroid.service.RunConditionMonitor.ACTION_SYNC_TRIGGER_FIRED;
+import static com.nutomic.syncthingandroid.service.RunConditionMonitor.EXTRA_BEGIN_ACTIVE_TIME_WINDOW;
+
+@RequiresApi(api = Build.VERSION_CODES.N)
+public class QuickSettingsTileSchedule extends TileService {
+    public QuickSettingsTileSchedule() {
+
+    }
+    private Context mContext;
+    @Inject
+    SharedPreferences mPreferences;
+
+    @Override
+    public void onStartListening() {
+        Tile tile = getQsTile();
+        if (tile != null) {
+            mContext = getApplication().getApplicationContext();
+            Resources res = mContext.getResources();
+            mPreferences = PreferenceManager.getDefaultSharedPreferences(mContext);
+
+            // look through running services to see whether the app is currently running
+            ActivityManager am = (ActivityManager) getSystemService(Context.ACTIVITY_SERVICE);
+            boolean syncthingRunning = false;
+            for (ActivityManager.RunningServiceInfo service : am.getRunningServices(Integer.MAX_VALUE)) {
+                if (SyncthingService.class.getName().equals(service.service.getClassName())) {
+                    syncthingRunning = true;
+                    break;
+                }
+            }
+            // disable tile if app is not running, schedule is off, or syncthing is force-started/stopped
+            if (!syncthingRunning || !mPreferences.getBoolean(Constants.PREF_RUN_ON_TIME_SCHEDULE,false)
+                    || mPreferences.getInt(Constants.PREF_BTNSTATE_FORCE_START_STOP, Constants.BTNSTATE_NO_FORCE_START_STOP) != Constants.BTNSTATE_NO_FORCE_START_STOP) {
+                tile.setState(Tile.STATE_UNAVAILABLE);
+                tile.setLabel(res.getString(R.string.qs_schedule_disabled));
+                tile.updateTile();
+                return;
+            }
+
+            tile.setState(Tile.STATE_ACTIVE);
+            tile.setLabel(res.getString(R.string.qs_schedule_label));
+            tile.updateTile();
+        }
+        super.onStartListening();
+    }
+
+    @Override
+    public void onClick() {
+        if (getQsTile().getState() == Tile.STATE_ACTIVE) {
+            LocalBroadcastManager localBroadcastManager = LocalBroadcastManager.getInstance(mContext);
+            Intent intent = new Intent(ACTION_SYNC_TRIGGER_FIRED);
+            intent.putExtra(EXTRA_BEGIN_ACTIVE_TIME_WINDOW, true);
+            localBroadcastManager.sendBroadcast(intent);
+        }
+    }
+}
