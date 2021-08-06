49d15aefd0784dc7303acacb87edcffaf1f6f152
Catfriend1 <16361913+Catfriend1@users.noreply.github.com>
2021-08-06 17:48:05 +0200

    Update setenv.cmd

--- setenv.cmd
+++ setenv.cmd
@@ -1,7 +1,7 @@
 @echo off
 REM 
 where git 2> NUL: || call :addGitToPath
-SET PATH=C:\Program Files\Android\Android Studio\jre\bin;C:\Program Files\Java\jdk1.8.0_241\bin;"%GIT_INSTALL_DIR%\bin";"%GIT_INSTALL_DIR%\cmd";%PATH%
+SET PATH=C:\Program Files\Android\Android Studio\jre\bin;"%GIT_INSTALL_DIR%\bin";"%GIT_INSTALL_DIR%\cmd";%PATH%
 REM 
 REM End of Script.
 REM 
