7a0c63694787ec6301d4481e2275ca5367da96c5
Catfriend1 <16361913+Catfriend1@users.noreply.github.com>
2021-09-24 13:34:48 +0200

    git remote add

--- update_fdroid_mirror_repo_from_main.cmd
+++ update_fdroid_mirror_repo_from_main.cmd
@@ -4,22 +4,23 @@ SET SCRIPT_PATH=%~dps0
 cd /d "%SCRIPT_PATH%"
 cls
 title Syncthing-Fork - Update F-Droid Mirror Repo from main
-REM 
+REM
 REM Runtime Variables.
 SET APP_BUILD_GRADLE=%~dps0app\build.gradle
-REM 
+REM
 call :runGit fetch --all
-REM 
+REM
+call :runGit remote add upstream git://github.com/Catfriend1/syncthing-android.git
 call :runGit pull upstream main
-REM 
+REM
 call :readVersionFromVersionsGradle
 echo [INFO] applicationId="%APPLICATION_ID%"
 echo [INFO] VERSION_NAME=[%VERSION_NAME%]
 echo [INFO] VERSION_CODE=[%VERSION_CODE%]
-REM 
+REM
 REM Write "versionName" and "versionCode" to "build.gradle".
 TYPE "%APP_BUILD_GRADLE%" 2>NUL: | psreplace "\sversionCode .*" " versionCode %VERSION_CODE%" | psreplace "\sversionName .*" " versionName `%VERSION_NAME%`" "%APP_BUILD_GRADLE%"
-REM 
+REM
 goto :eof
 
 
@@ -35,30 +36,30 @@ REM ====================
 REM FUNCTION BLOCK START
 REM ====================
 :readVersionFromVersionsGradle
-REM 
+REM
 REM Get "applicationId"
 SET APPLICATION_ID=
 FOR /F "tokens=2 delims= " %%A IN ('type "%SCRIPT_PATH%app\build.gradle" 2^>^&1 ^| findstr "applicationId"') DO SET APPLICATION_ID=%%A
 SET APPLICATION_ID=%APPLICATION_ID:"=%
-REM 
+REM
 REM Get "versionMajor"
 SET VERSION_MAJOR=
 FOR /F "tokens=2 delims== " %%A IN ('type "%SCRIPT_PATH%app\versions.gradle" 2^>^&1 ^| findstr "versionMajor"') DO SET VERSION_MAJOR=%%A
 SET VERSION_MAJOR=%VERSION_MAJOR:"=%
 REM echo [INFO] versionMajor="%VERSION_MAJOR%"
-REM 
+REM
 REM Get "versionMinor"
 SET VERSION_MINOR=
 FOR /F "tokens=2 delims== " %%A IN ('type "%SCRIPT_PATH%app\versions.gradle" 2^>^&1 ^| findstr "versionMinor"') DO SET VERSION_MINOR=%%A
 SET VERSION_MINOR=%VERSION_MINOR:"=%
 REM echo [INFO] versionMinor="%VERSION_MINOR%"
-REM 
+REM
 REM Get "versionPatch"
 SET VERSION_PATCH=
 FOR /F "tokens=2 delims== " %%A IN ('type "%SCRIPT_PATH%app\versions.gradle" 2^>^&1 ^| findstr "versionPatch"') DO SET VERSION_PATCH=%%A
 SET VERSION_PATCH=%VERSION_PATCH:"=%
 REM echo [INFO] versionPatch="%VERSION_PATCH%"
-REM 
+REM
 REM Get "versionWrapper"
 SET VERSION_WRAPPER=
 FOR /F "tokens=2 delims== " %%A IN ('type "%SCRIPT_PATH%app\versions.gradle" 2^>^&1 ^| findstr "versionWrapper"') DO SET VERSION_WRAPPER=%%A
@@ -66,24 +67,24 @@ SET VERSION_WRAPPER=%VERSION_WRAPPER:"=%
 REM echo [INFO] versionWrapper="%VERSION_WRAPPER%"
 REM
 SET VERSION_NAME=%VERSION_MAJOR%.%VERSION_MINOR%.%VERSION_PATCH%.%VERSION_WRAPPER%
-REM 
+REM
 REM Calculate "versionCode".
 SET VERSION_CODE_MAJOR=%VERSION_MAJOR%
 REM 
 call :addTrailingZerosToVar %VERSION_MINOR%
 SET VERSION_CODE_MINOR=%ATZTV_PADDED%
-REM 
+REM
 call :addTrailingZerosToVar %VERSION_MINOR%
 SET VERSION_CODE_MINOR=%ATZTV_PADDED%
-REM 
+REM
 call :addTrailingZerosToVar %VERSION_PATCH%
 SET VERSION_CODE_PATCH=%ATZTV_PADDED%
-REM 
+REM
 call :addTrailingZerosToVar %VERSION_WRAPPER%
 SET VERSION_CODE_WRAPPER=%ATZTV_PADDED%
-REM 
+REM
 SET VERSION_CODE=%VERSION_CODE_MAJOR%%VERSION_CODE_MINOR%%VERSION_CODE_PATCH%%VERSION_CODE_WRAPPER%
-REM 
+REM
 goto :eof
 
 
