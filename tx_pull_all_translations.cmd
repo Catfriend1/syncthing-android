a2187d72904ba13c271dbf216a0793238e73fa6e
Catfriend1 <16361913+Catfriend1@users.noreply.github.com>
2022-09-29 21:17:43 +0200

    Import translations, omit review status

--- tx_pull_all_translations.cmd
+++ tx_pull_all_translations.cmd
@@ -6,9 +6,9 @@ REM
 SET FORCE_FLAG=-f
 REM 
 echo Pulling all reviewed translations ...
-tx pull -a --parallel --mode reviewed %FORCE_FLAG% -r "syncthing-android-1.stringsxml"
-tx pull -a --parallel --mode reviewed %FORCE_FLAG% -r "syncthing-android-1.description_fulltxt"
-tx pull -a --parallel --mode reviewed %FORCE_FLAG% -r "syncthing-android-1.description_shorttxt"
-tx pull -a --parallel --mode reviewed %FORCE_FLAG% -r "syncthing-android-1.titletxt"
+tx pull -a --parallel %FORCE_FLAG% -r "syncthing-android-1.stringsxml"
+tx pull -a --parallel %FORCE_FLAG% -r "syncthing-android-1.description_fulltxt"
+tx pull -a --parallel %FORCE_FLAG% -r "syncthing-android-1.description_shorttxt"
+tx pull -a --parallel %FORCE_FLAG% -r "syncthing-android-1.titletxt"
 REM 
 pause
