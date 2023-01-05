5c8e15e2d4cce8d7fb1c37c7f547160a8c19459f
Catfriend1 <16361913+Catfriend1@users.noreply.github.com>
2023-01-05 20:42:50 +0100

    Build syncthing x86_64 so variant, fixed in SyncthingNative v1.23.0 (fixes #583) (#949)
    * Update syncthing
    
    * Update build-syncthing.py

--- syncthing/build-syncthing.py
+++ syncthing/build-syncthing.py
@@ -47,6 +47,12 @@ BUILD_TARGETS = [
         'goarch': '386',
         'jni_dir': 'x86',
         'cc': 'i686-linux-android{}-clang',
+    },
+    {
+        'arch': 'x86_64',
+        'goarch': 'amd64',
+        'jni_dir': 'x86_64',
+        'cc': 'x86_64-linux-android{}-clang',
     }
 ]
 
@@ -399,12 +405,5 @@ for target in BUILD_TARGETS:
     os.rename(os.path.join(syncthing_dir, 'syncthing'), target_artifact)
 
     print('*** Finished build for', target['arch'])
-
-print('Copy x86 artifact to x86_64 folder, workaround for issue #583')
-target_dir = os.path.join(project_dir, 'app', 'src', 'main', 'jniLibs', 'x86_64')
-if not os.path.isdir(target_dir):
-    os.makedirs(target_dir)
-shutil.copy(os.path.join(project_dir, 'app', 'src', 'main', 'jniLibs', 'x86', FILENAME_SYNCTHING_BINARY),
-        os.path.join(target_dir, FILENAME_SYNCTHING_BINARY))
 
 print('All builds finished')
