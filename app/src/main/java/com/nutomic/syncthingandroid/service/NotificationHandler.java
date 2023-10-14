9d85a1c5341f6a92888b3019ebdd31ab54938156
Catfriend1 <16361913+Catfriend1@users.noreply.github.com>
2023-10-15 00:53:38 +0200

    Fix lint: NewApi startForeground

--- app/src/main/java/com/nutomic/syncthingandroid/service/NotificationHandler.java
+++ app/src/main/java/com/nutomic/syncthingandroid/service/NotificationHandler.java
@@ -212,7 +212,11 @@ public class NotificationHandler {
         if (!appShutdownInProgress) {
             if (startForegroundService) {
                 Log.v(TAG, "Starting foreground service or updating notification");
-                service.startForeground(idToShow, builder.build(), ServiceInfo.FOREGROUND_SERVICE_TYPE_DATA_SYNC);
+                if (Build.VERSION.SDK_INT > Build.VERSION_CODES.P) {
+                    service.startForeground(idToShow, builder.build(), ServiceInfo.FOREGROUND_SERVICE_TYPE_DATA_SYNC);
+                } else {
+                    service.startForeground(idToShow, builder.build());
+                }
             } else {
                 Log.v(TAG, "Updating notification");
                 mNotificationManager.notify(idToShow, builder.build());
