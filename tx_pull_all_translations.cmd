f13ff8daea6c13cd26568f49fcc4a668dd4dd34c
André Colomb <github.com@andre.colomb.de>
2023-11-12 12:14:26 +0100

    Fix Transifex config to make pull script work again. (#1031)
    * Migrate to new tx config file format.
    
    * Remove obsolete CLI flags for resource ID and parallel.
    
    Ref. https://developers.transifex.com/docs/using-the-client#pulling-files-from-transifex
    
    * Switch to hostname as specified in Transifex docs.
    
    * Import translations from Transifex.
    
    Credits to @acolomb

--- tx_pull_all_translations.cmd
+++ tx_pull_all_translations.cmd
@@ -6,10 +6,10 @@ REM
 SET FORCE_FLAG=-f
 REM 
 echo Pulling all reviewed translations ...
-tx pull -a --parallel %FORCE_FLAG% -r "syncthing-android-1.stringsxml"
-tx pull -a --parallel %FORCE_FLAG% -r "syncthing-android-1.description_fulltxt"
-tx pull -a --parallel %FORCE_FLAG% -r "syncthing-android-1.description_shorttxt"
-tx pull -a --parallel %FORCE_FLAG% -r "syncthing-android-1.titletxt"
+tx pull -a %FORCE_FLAG% "syncthing-android-1.stringsxml"
+tx pull -a %FORCE_FLAG% "syncthing-android-1.description_fulltxt"
+tx pull -a %FORCE_FLAG% "syncthing-android-1.description_shorttxt"
+tx pull -a %FORCE_FLAG% "syncthing-android-1.titletxt"
 REM
 RD /S /Q "app\src\main\play\listings\nl_BE" 2>NUL:
 REM 
