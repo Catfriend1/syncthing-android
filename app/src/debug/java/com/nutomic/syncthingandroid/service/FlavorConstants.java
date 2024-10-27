e7c099378ac702abc45f34cccba7ebc8906d65cc
Catfriend1 <16361913+Catfriend1@users.noreply.github.com>
2024-10-27 20:05:53 +0100

    fix lint

--- app/src/debug/java/com/nutomic/syncthingandroid/service/FlavorConstants.java
+++ app/src/debug/java/com/nutomic/syncthingandroid/service/FlavorConstants.java
@@ -1,9 +1,11 @@
 package com.nutomic.syncthingandroid.service;
 
+import android.annotation.TargetApi;
 import android.content.pm.ServiceInfo;
 
 public class FlavorConstants {
 
+    @TargetApi(34)
     public static final Integer FOREGROUND_SERVICE_TYPE         = ServiceInfo.FOREGROUND_SERVICE_TYPE_SPECIAL_USE;
 
 }
--- app/src/release/java/com/nutomic/syncthingandroid/service/FlavorConstants.java
+++ app/src/release/java/com/nutomic/syncthingandroid/service/FlavorConstants.java
@@ -1,5 +1,6 @@
 package com.nutomic.syncthingandroid.service;
 
+import android.annotation.TargetApi;
 import android.content.pm.ServiceInfo;
 
 public class FlavorConstants {
