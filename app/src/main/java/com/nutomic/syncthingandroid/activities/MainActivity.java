c70606def3cf4cfca0b5f7c85e305387fc8827a2
Catfriend1 <16361913+Catfriend1@users.noreply.github.com>
2024-12-10 23:40:31 +0100

    Update MainActivity.java

--- app/src/main/java/com/nutomic/syncthingandroid/activities/MainActivity.java
+++ app/src/main/java/com/nutomic/syncthingandroid/activities/MainActivity.java
@@ -4,7 +4,6 @@ import android.Manifest;
 import android.annotation.SuppressLint;
 import android.app.Dialog;
 import android.content.ComponentName;
-import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
