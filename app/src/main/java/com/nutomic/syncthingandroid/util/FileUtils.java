b84c4fd51fa6d1494b0f783e7dfa5c9e5dcf8942
Catfriend1 <16361913+Catfriend1@users.noreply.github.com>
2025-05-08 14:08:02 +0200

    Update FileUtils.java

--- app/src/main/java/com/nutomic/syncthingandroid/util/FileUtils.java
+++ app/src/main/java/com/nutomic/syncthingandroid/util/FileUtils.java
@@ -211,6 +211,14 @@ public class FileUtils {
      * API for getExternalFilesDirs(): 19+ (KITKAT+)
      * API for getExternalMediaDirs(): 21+ (LOLLIPOP+)
      */
+    public static android.net.Uri convertFromDocumentUriToTreeUri(Uri documentUri) {
+        // IN: content://com.android.externalstorage.documents/document/primary%3AAndroid%2Fmedia%2Fcom.github.catfriend1.syncthingandroid.debug
+        // OUT: content://com.android.externalstorage.documents/tree/primary%3AAndroid%2Fmedia%2Fcom.github.catfriend1.syncthingandroid.debug
+        String authority = documentUri.getAuthority();
+        String documentId = DocumentsContract.getDocumentId(documentUri);
+        return DocumentsContract.buildTreeDocumentUri(authority, documentId);
+    }
+
     public static android.net.Uri getExternalFilesDirUri(Context context, ExternalStorageDirType extDirType) {
         try {
             File externalFilesDir = getExternalFilesDir(context, extDirType, null);
