9e39004cf2107d3d67c577c69b0f28c681bdc661
Catfriend1 <16361913+Catfriend1@users.noreply.github.com>
2025-05-10 19:52:49 +0200

    kotlin & compose compiler version

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,11 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 
+plugins {
+    alias(libs.plugins.aboutLibraries) apply false
+    alias(libs.plugins.compose.compiler) apply false
+    alias(libs.plugins.kotlin.android) apply false
+}
+
 buildscript {
     extra.apply {
         // Cannot be called "ndkVersion" as that leads to naming collision
