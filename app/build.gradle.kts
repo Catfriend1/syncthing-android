6bf97dd67c4f80d78de74e30565a0834d0ea3f4b
Catfriend1 <16361913+Catfriend1@users.noreply.github.com>
2025-05-10 18:43:46 +0200

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -31,6 +31,13 @@ dependencies {
     implementation(libs.aboutlibraries.compose.m2) // Material 2
     implementation(libs.aboutlibraries.compose.m3) // Material 3
     implementation(libs.aboutlibraries.core)
+
+    implementation("androidx.activity:activity-compose:1.9.0")
+    implementation("androidx.compose.ui:ui:1.6.5")
+    implementation("androidx.compose.foundation:foundation:1.6.5")
+    implementation("androidx.compose.material:material:1.6.5")
+    implementation("androidx.compose.material3:material3:1.2.1")
+    implementation("androidx.compose.ui:ui-tooling-preview:1.6.5")
 }
 
 android {
