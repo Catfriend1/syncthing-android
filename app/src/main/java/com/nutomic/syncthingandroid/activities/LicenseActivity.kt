2947704f747519121567768426be495558742213
Catfriend1 <16361913+Catfriend1@users.noreply.github.com>
2025-05-10 22:11:57 +0200

    Update LicenseActivity.kt

--- app/src/main/java/com/nutomic/syncthingandroid/activities/LicenseActivity.kt
+++ app/src/main/java/com/nutomic/syncthingandroid/activities/LicenseActivity.kt
@@ -6,7 +6,13 @@ import androidx.activity.compose.LocalOnBackPressedDispatcherOwner
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.*
+import androidx.compose.material.IconButton
+import androidx.compose.material.Icon
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Scaffold
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.material.TopAppBar
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.runtime.Composable
