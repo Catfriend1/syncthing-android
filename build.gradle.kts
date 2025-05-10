ac2fe4859833531eeea8ff64cc42f98cc3641253
Catfriend1 <16361913+Catfriend1@users.noreply.github.com>
2025-05-10 22:20:15 +0200

    1

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,11 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 
+plugins {
+    alias(libs.plugins.compose.compiler) apply false
+    alias(libs.plugins.kotlin.android) apply false
+    alias(libs.plugins.kotlin.kapt) apply false
+}
+
 buildscript {
     extra.apply {
         // Cannot be called "ndkVersion" as that leads to naming collision
