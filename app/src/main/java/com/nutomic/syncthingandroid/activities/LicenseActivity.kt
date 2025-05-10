0742807ff02a78402ad94f8bd5f583af5acc0dcc
Catfriend1 <16361913+Catfriend1@users.noreply.github.com>
2025-05-10 17:51:29 +0200

    Update LicenseActivity.kt

--- app/src/main/java/com/nutomic/syncthingandroid/activities/LicenseActivity.kt
+++ app/src/main/java/com/nutomic/syncthingandroid/activities/LicenseActivity.kt
@@ -3,8 +3,6 @@ package com.nutomic.syncthingandroid.activities
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.activity.ComponentActivity
-import androidx.compose.runtime.Composable
-import androidx.activity.compose.setContent
 import com.mikepenz.aboutlibraries.LibsBuilder
 import com.nutomic.syncthingandroid.R
 
