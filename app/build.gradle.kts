6109722b8124680b63bf32e5c09d00586bd8c4fc
Catfriend1 <16361913+Catfriend1@users.noreply.github.com>
2025-05-10 19:10:39 +0200

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,6 +10,12 @@ dependencies {
     androidTestImplementation("androidx.annotation:annotation:1.2.0")
     androidTestImplementation("androidx.test:rules:1.6.1")
     annotationProcessor("com.google.dagger:dagger-compiler:2.56.2")
+    implementation("androidx.activity:activity-compose:1.10.1")
+    implementation("androidx.compose.ui:ui:1.8.1")
+    implementation("androidx.compose.foundation:foundation:1.8.1")
+    implementation("androidx.compose.material:material:1.8.1")
+    implementation("androidx.compose.material3:material3:1.3.2")
+    implementation("androidx.compose.ui:ui-tooling-preview:1.8.1")
     implementation("androidx.preference:preference:1.2.1")
     implementation("androidx.constraintlayout:constraintlayout:2.0.4")
     implementation("androidx.core:core:1.16.0")
@@ -32,12 +38,6 @@ dependencies {
     implementation(libs.aboutlibraries.compose.m3) // Material 3
     implementation(libs.aboutlibraries.core)
 
-    implementation("androidx.activity:activity-compose:1.10.1")
-    implementation("androidx.compose.ui:ui:1.8.1")
-    implementation("androidx.compose.foundation:foundation:1.8.1")
-    implementation("androidx.compose.material:material:1.8.1")
-    implementation("androidx.compose.material3:material3:1.3.2")
-    implementation("androidx.compose.ui:ui-tooling-preview:1.8.1")
 }
 
 android {
