820db86702747997167130115824e6aa681ce367
Catfriend1 <16361913+Catfriend1@users.noreply.github.com>
2025-05-13 15:00:18 +0200

    build/plugin: Remove com.github.ben-manes.versions
    Replaced by GitHub's dependabot

--- build.gradle.kts
+++ build.gradle.kts
@@ -26,7 +26,6 @@ buildscript {
 
     dependencies {
         classpath("com.android.tools.build:gradle:8.6.1")
-        classpath("com.github.ben-manes:gradle-versions-plugin:0.52.0")
 
         // NOTE: Do not place your application dependencies here; they belong
         // in the individual module build.gradle files
