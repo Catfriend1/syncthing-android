3836b470cdc4a2ff2e2c36cb8a2d00593e32f9f4
Catfriend1 <16361913+Catfriend1@users.noreply.github.com>
2025-05-13 15:03:48 +0200

    gradle: Update 8.9 > 8.10.2
    fix lint: quick setting tiles (minApi 24), NotificationHandler FLAG_IMMUTABLE (minApi 23)

--- build.gradle.kts
+++ build.gradle.kts
@@ -25,7 +25,7 @@ buildscript {
     }
 
     dependencies {
-        classpath("com.android.tools.build:gradle:8.6.1")
+        classpath("com.android.tools.build:gradle:8.8.2")
 
         // NOTE: Do not place your application dependencies here; they belong
         // in the individual module build.gradle files
