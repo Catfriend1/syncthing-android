7602a05cb974adc728522b1d447dc78fe18354aa
Catfriend1 <16361913+Catfriend1@users.noreply.github.com>
2025-06-04 18:07:49 +0200

    Update SyncthingService.java

--- app/src/main/java/com/nutomic/syncthingandroid/service/SyncthingService.java
+++ app/src/main/java/com/nutomic/syncthingandroid/service/SyncthingService.java
@@ -29,8 +29,6 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Iterator;
