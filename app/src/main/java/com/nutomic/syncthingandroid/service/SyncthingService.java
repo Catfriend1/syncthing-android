6de73ebb5856f0bc4ad0de74146b21ddabeafa4d
Catfriend1 <16361913+Catfriend1@users.noreply.github.com>
2025-06-04 09:34:10 +0200

    Update SyncthingService.java

--- app/src/main/java/com/nutomic/syncthingandroid/service/SyncthingService.java
+++ app/src/main/java/com/nutomic/syncthingandroid/service/SyncthingService.java
@@ -39,6 +39,13 @@ import java.util.Set;
 
 import javax.inject.Inject;
 
+import net.lingala.zip4j.ZipFile;
+import net.lingala.zip4j.model.ZipParameters;
+import net.lingala.zip4j.model.enums.CompressionLevel;
+import net.lingala.zip4j.model.enums.CompressionMethod;
+import net.lingala.zip4j.model.enums.EncryptionMethod;
+import net.lingala.zip4j.model.enums.AesKeyStrength;
+
 /**
  * Holds the native syncthing instance and provides an API to access it.
  */
