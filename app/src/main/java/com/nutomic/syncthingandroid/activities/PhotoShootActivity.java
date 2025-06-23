6c3bddb8eea910791a10d7e20e63c55dab990ca4
Catfriend1 <16361913+Catfriend1@users.noreply.github.com>
2025-06-23 16:11:29 +0200

    Update PhotoShootActivity.java

--- app/src/main/java/com/nutomic/syncthingandroid/activities/PhotoShootActivity.java
+++ app/src/main/java/com/nutomic/syncthingandroid/activities/PhotoShootActivity.java
@@ -81,12 +81,6 @@ public class PhotoShootActivity extends ThemedAppCompatActivity {
             return;
         }
 
-        // Make notification bar transparent (API level 21+)
-        if (Build.VERSION.SDK_INT <= Build.VERSION_CODES.Q) {
-            getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE |
-                    View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
-        }
-
         // Show photo shoot intro UI to request required permissions.
         setContentView(R.layout.activity_photo_shoot_intro);
 
