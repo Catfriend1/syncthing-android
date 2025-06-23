cacad73e0ff5b962d3d6a748f1d7981ede1192b1
Catfriend1 <16361913+Catfriend1@users.noreply.github.com>
2025-06-23 04:37:02 +0200

    Update LicenseActivity.kt

--- app/src/main/java/com/nutomic/syncthingandroid/activities/LicenseActivity.kt
+++ app/src/main/java/com/nutomic/syncthingandroid/activities/LicenseActivity.kt
@@ -2,15 +2,15 @@ package com.nutomic.syncthingandroid.activities
 
 import android.os.Build
 import android.os.Bundle
-import android.util.TypedValue
-import android.view.View
-import android.view.ViewGroup
 
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.LocalOnBackPressedDispatcherOwner
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.statusBars
+import androidx.compose.foundation.layout.windowInsetsPadding
 import androidx.compose.material.IconButton
 import androidx.compose.material.Icon
 import androidx.compose.material.MaterialTheme
@@ -26,7 +26,6 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
-import androidx.core.content.ContextCompat
 
 import com.mikepenz.aboutlibraries.ui.compose.rememberLibraries
 import com.mikepenz.aboutlibraries.ui.compose.LibrariesContainer
@@ -38,42 +37,6 @@ class LicenseActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             LicenseScreen()
-        }
-    }
-
-    override fun onStart() {
-        super.onStart()
-
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.VANILLA_ICE_CREAM) {
-            val contentView = findViewById<ViewGroup>(android.R.id.content)
-            if (contentView.childCount > 0) {
-                val topLevel = contentView.getChildAt(0)
-                when (topLevel) {
-                    is ViewGroup -> addSpacerIfNeeded(topLevel)
-                }
-            }
-        }
-    }
-
-    private fun addSpacerIfNeeded(parent: ViewGroup) {
-        val statusBarSpacer = View(this).apply {
-            layoutParams = ViewGroup.LayoutParams(
-                ViewGroup.LayoutParams.MATCH_PARENT,
-                getStatusBarHeight()
-            )
-            setBackgroundColor(ContextCompat.getColor(this@LicenseActivity, R.color.primary))
-        }
-        parent.addView(statusBarSpacer, 0)
-    }
-
-    private fun getStatusBarHeight(): Int {
-        val typedValue = TypedValue()
-        return if (theme.resolveAttribute(android.R.attr.actionBarSize, typedValue, true)) {
-            TypedValue.complexToDimensionPixelSize(
-                typedValue.data, resources.displayMetrics
-            )
-        } else {
-            0
         }
     }
 }
@@ -90,6 +53,7 @@ fun LicenseScreen() {
             }
 
             Scaffold(
+                modifier = Modifier.windowInsetsPadding(WindowInsets.statusBars),
                 topBar = {
                     TopAppBar(
                         title = { Text(stringResource(id = R.string.open_source_licenses_title)) },
