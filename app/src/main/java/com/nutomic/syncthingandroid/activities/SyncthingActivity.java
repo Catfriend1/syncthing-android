14383ad19547c662ce3402dc2486e88fe44640f5
Catfriend1 <16361913+Catfriend1@users.noreply.github.com>
2025-06-23 15:50:39 +0200

    Update SyncthingActivity.java

--- app/src/main/java/com/nutomic/syncthingandroid/activities/SyncthingActivity.java
+++ app/src/main/java/com/nutomic/syncthingandroid/activities/SyncthingActivity.java
@@ -4,7 +4,6 @@ import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.content.ServiceConnection;
-import android.os.Build;
 import android.os.Bundle;
 import android.os.IBinder;
 
