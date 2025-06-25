834d1855125b4f88b60bca872ed93558b5db9212
Catfriend1 <16361913+Catfriend1@users.noreply.github.com>
2025-06-25 14:21:22 +0200

    Update build-syncthing.py

--- syncthing/build-syncthing.py
+++ syncthing/build-syncthing.py
@@ -332,12 +332,13 @@ for target in BUILD_TARGETS:
         target['cc'].format(min_sdk),
     )
 
+    # See why "-checklinkname=0" is required: https://github.com/wlynxg/anet?tab=readme-ov-file#how-to-build-with-go-1230-or-later 
     environ = os.environ.copy()
     environ.update({
         'GOPATH': module_dir,
         'GO111MODULE': 'on',
         'CGO_ENABLED': '1',
-        'EXTRA_LDFLAGS': '-buildid=',
+        'EXTRA_LDFLAGS': '-buildid= -checklinkname=0',
     })
 
     subprocess.check_call([go_bin, 'mod', 'download'], cwd=syncthing_dir)
