c7736df63edd8b49695cee2e3539dc97c126304a
Catfriend1 <16361913+Catfriend1@users.noreply.github.com>
2025-06-26 23:04:07 +0200

    Update update_fdroid_branch_from_main.cmd

--- fdroid/update_fdroid_branch_from_main.cmd
+++ fdroid/update_fdroid_branch_from_main.cmd
@@ -1,26 +1,32 @@
 @echo off
 setlocal enabledelayedexpansion
-SET SCRIPT_PATH=%~dps0
-cd /d "%SCRIPT_PATH%.."
-cls
 title Update "fdroid" with "main" branch
-REM
-REM Runtime Variables.
-SET APP_BUILD_GRADLE=%~dps0app\build.gradle.kts
-REM
+::
+:: Relauch from local drive.
+SET "RELAUNCH_PATH=%TEMP%\%~nx0"
+IF /I NOT "%~dpnx0" == "%RELAUNCH_PATH%" SET "PROJECT_ROOT=%~dp0.." & copy /y "%~dpnx0" "%RELAUNCH_PATH%" >NUL: & echo [INFO] Relaunch from local drive & call "%RELAUNCH_PATH%" %1 %2 %3 %4 %5 %6 %7 %8 %9 & goto :eof
+::
+echo [INFO] Relaunched.
+cd /d "%PROJECT_ROOT%"
+::
+:: Runtime Variables.
+SET "APP_BUILD_GRADLE=app\build.gradle.kts"
+::
+:: Consts.
+SET DRY_RUN=0
+::
 call :runGit fetch --all
-REM
-call :runGit checkout fdroid
-call :runGit pull origin main
-REM
+::
+IF "%DRY_RUN%" == "0" call :runGit checkout fdroid
+IF "%DRY_RUN%" == "0" call :runGit pull origin main
+::
 call :readVersionFromVersionsGradle
-echo [INFO] applicationId="%APPLICATION_ID%"
 echo [INFO] VERSION_NAME=[%VERSION_NAME%]
 echo [INFO] VERSION_CODE=[%VERSION_CODE%]
-REM
-REM Write "versionName" and "versionCode" to "build.gradle".
+::
+:: Write "versionName" and "versionCode" to "build.gradle".
 TYPE "%APP_BUILD_GRADLE%" 2>NUL: | psreplace "\sversionCode = .*" " versionCode = %VERSION_CODE%" | psreplace "\sversionName = .*" " versionName = `%VERSION_NAME%`" "%APP_BUILD_GRADLE%"
-REM
+::
 goto :eof
 
 
@@ -32,59 +38,54 @@ goto :eof
 
 
 
-REM ====================
-REM FUNCTION BLOCK START
-REM ====================
+:: ====================
+:: FUNCTION BLOCK START
+:: ====================
 :readVersionFromVersionsGradle
-REM
-REM Get "applicationId"
-SET APPLICATION_ID=
-FOR /F "tokens=3 delims=) " %%A IN ('type "%SCRIPT_PATH%app\build.gradle.kts" 2^>^&1 ^| findstr "applicationId"') DO SET APPLICATION_ID=%%A
-SET APPLICATION_ID=%APPLICATION_ID:"=%
-REM
-REM Get "versionMajor"
+::
+:: Get "versionMajor"
 SET VERSION_MAJOR=
-FOR /F "tokens=2 delims==) " %%A IN ('type "%SCRIPT_PATH%build.gradle.kts" 2^>^&1 ^| findstr "versionMajor"') DO SET VERSION_MAJOR=%%A
+FOR /F "tokens=2 delims==) " %%A IN ('type "build.gradle.kts" 2^>^&1 ^| findstr "versionMajor"') DO SET VERSION_MAJOR=%%A
 SET VERSION_MAJOR=%VERSION_MAJOR:"=%
-REM echo [INFO] versionMajor="%VERSION_MAJOR%"
-REM
-REM Get "versionMinor"
+:: echo [INFO] versionMajor="%VERSION_MAJOR%"
+::
+:: Get "versionMinor"
 SET VERSION_MINOR=
-FOR /F "tokens=2 delims==) " %%A IN ('type "%SCRIPT_PATH%build.gradle.kts" 2^>^&1 ^| findstr "versionMinor"') DO SET VERSION_MINOR=%%A
+FOR /F "tokens=2 delims==) " %%A IN ('type "build.gradle.kts" 2^>^&1 ^| findstr "versionMinor"') DO SET VERSION_MINOR=%%A
 SET VERSION_MINOR=%VERSION_MINOR:"=%
-REM echo [INFO] versionMinor="%VERSION_MINOR%"
-REM
-REM Get "versionPatch"
+:: echo [INFO] versionMinor="%VERSION_MINOR%"
+::
+:: Get "versionPatch"
 SET VERSION_PATCH=
-FOR /F "tokens=2 delims==) " %%A IN ('type "%SCRIPT_PATH%build.gradle.kts" 2^>^&1 ^| findstr "versionPatch"') DO SET VERSION_PATCH=%%A
+FOR /F "tokens=2 delims==) " %%A IN ('type "build.gradle.kts" 2^>^&1 ^| findstr "versionPatch"') DO SET VERSION_PATCH=%%A
 SET VERSION_PATCH=%VERSION_PATCH:"=%
-REM echo [INFO] versionPatch="%VERSION_PATCH%"
-REM
-REM Get "versionWrapper"
+:: echo [INFO] versionPatch="%VERSION_PATCH%"
+::
+:: Get "versionWrapper"
 SET VERSION_WRAPPER=
-FOR /F "tokens=2 delims==) " %%A IN ('type "%SCRIPT_PATH%build.gradle.kts" 2^>^&1 ^| findstr "versionWrapper"') DO SET VERSION_WRAPPER=%%A
+FOR /F "tokens=2 delims==) " %%A IN ('type "build.gradle.kts" 2^>^&1 ^| findstr "versionWrapper"') DO SET VERSION_WRAPPER=%%A
 SET VERSION_WRAPPER=%VERSION_WRAPPER:"=%
-REM echo [INFO] versionWrapper="%VERSION_WRAPPER%"
-REM
+:: echo [INFO] versionWrapper="%VERSION_WRAPPER%"
+::
 SET VERSION_NAME=%VERSION_MAJOR%.%VERSION_MINOR%.%VERSION_PATCH%.%VERSION_WRAPPER%
-REM
-REM Calculate "versionCode".
+::
+:: Calculate "versionCode".
 SET VERSION_CODE_MAJOR=%VERSION_MAJOR%
-REM 
+:: 
 call :addTrailingZerosToVar %VERSION_MINOR%
 SET VERSION_CODE_MINOR=%ATZTV_PADDED%
-REM
+::
 call :addTrailingZerosToVar %VERSION_MINOR%
 SET VERSION_CODE_MINOR=%ATZTV_PADDED%
-REM
+::
 call :addTrailingZerosToVar %VERSION_PATCH%
 SET VERSION_CODE_PATCH=%ATZTV_PADDED%
-REM
+::
 call :addTrailingZerosToVar %VERSION_WRAPPER%
 SET VERSION_CODE_WRAPPER=%ATZTV_PADDED%
-REM
+::
 SET VERSION_CODE=%VERSION_CODE_MAJOR%%VERSION_CODE_MINOR%%VERSION_CODE_PATCH%%VERSION_CODE_WRAPPER%
-REM
+::
 goto :eof
 
 
@@ -105,6 +106,6 @@ goto :eof
 :pauseExit
 pause
 goto :eof
-REM ==================
-REM FUNCTION BLOCK END
-REM ==================
+:: ==================
+:: FUNCTION BLOCK END
+:: ==================
