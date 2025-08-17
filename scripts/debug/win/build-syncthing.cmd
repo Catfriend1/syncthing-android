585febb655b8805c185292dea8b6feeb134ffceb
Catfriend1 <16361913+Catfriend1@users.noreply.github.com>
2025-08-17 19:33:16 +0200

    Go: 1.24.1 > 1.25.0

--- scripts/debug/win/build-syncthing.cmd
+++ scripts/debug/win/build-syncthing.cmd
@@ -9,7 +9,7 @@ SET BUILD_USER=reproducible-build
 SET SOURCE_DATE_EPOCH=1
 SET SYNCTHING_VERSION=-version "v2.0.0-reproducibleBuildTest"
 ::
-where go 2>NUL: || SET "PATH=%PATH%;F:\GitHub\syncthing-android-prereq\go_1.24.1\bin"
+where go 2>NUL: || SET "PATH=%PATH%;F:\GitHub\syncthing-android-prereq\go_1.25.0\bin"
 where go >NUL: || (echo "[ERROR] go missing on PATH." & goto :eof)
 ::
 cd /d "F:\GitHub\syncthing" || (echo "[ERROR] cd FAILED." & goto :eof)
