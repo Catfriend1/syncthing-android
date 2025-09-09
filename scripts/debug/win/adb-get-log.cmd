c8122f69de6334f5191aca8cf634e21c2a5dcec0
Catfriend1 <16361913+Catfriend1@users.noreply.github.com>
2025-09-09 22:13:44 +0200

    Update adb-get-log.cmd

--- scripts/debug/win/adb-get-log.cmd
+++ scripts/debug/win/adb-get-log.cmd
@@ -7,11 +7,13 @@ SET /P PACKAGE_NAME=< "%PROJECT_ROOT%\scripts\debug\package_id.txt"
 ::
 where adb >NUL: 2>&1 || (echo [ERROR] adb is missing. Please install it first. Stop. & goto :eof)
 ::
+echo [INFO] Looking for app [%PACKAGE_NAME%]
 FOR /F "delims=" %%I IN ('adb shell pidof %PACKAGE_NAME%') DO SET PID=%%I
-IF NOT DEFINED PID echo [ERROR] Syncthing-Fork is NOT installed. Please install it first. Stop. & goto :eof
+IF NOT DEFINED PID echo [ERROR] Syncthing-Fork DEBUG is NOT installed. & FOR /F "delims=" %%I IN ('adb shell pidof %PACKAGE_NAME:.debug=%') DO SET PID=%%I
+IF NOT DEFINED PID echo [ERROR] Syncthing-Fork RELEASE is NOT installed. Please install it first. Stop. & goto :eof
 ::
 echo [INFO] Found PID: %PID%
-timeout /nobreak 1 >NUL:
+timeout /nobreak 3 >NUL:
 adb logcat --pid=%PID% *:V
 ::
 goto :eof
