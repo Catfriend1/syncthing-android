103cbdc1fb8286a044c5d51608e76ef360149696
Catfriend1 <16361913+Catfriend1@users.noreply.github.com>
2025-09-24 09:43:46 +0200

    Update FolderPickerActivity.java

--- app/src/main/java/com/nutomic/syncthingandroid/activities/FolderPickerActivity.java
+++ app/src/main/java/com/nutomic/syncthingandroid/activities/FolderPickerActivity.java
@@ -99,7 +99,6 @@ public class FolderPickerActivity extends SyncthingActivity
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        // No dependency injection needed - this activity doesn't inject any dependencies
 
         setContentView(R.layout.activity_folder_picker);
         mCurrentPath = findViewById(R.id.currentPath);
